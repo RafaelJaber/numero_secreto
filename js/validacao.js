f6d93261a1a5bc39762911538a43f224afa79d3d
Rafael Jáber <rafael.jaber@hotmail.com>
2023-05-15 23:47:29 -0300

    Implementado o botão para reiniciar a aplicação.

--- js/validacao.js
+++ js/validacao.js
@@ -17,6 +17,8 @@ function veriricaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era: ${numeroSecreto}</h3>
+            
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero < numeroSecreto) {
             elementoBox.innerHTML += `
@@ -37,3 +39,8 @@ function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor;
 }
 
+document.body.addEventListener('click', e => {
+    if (e.target.id === 'jogar-novamente') {
+        window.location.reload();
+    }
+})
